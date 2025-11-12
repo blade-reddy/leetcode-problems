d252c07051721ee4f53b5cdbc02e12cba687d6c1
blade-reddy <yashwanthr.choodi@gmail.com>
2025-11-12 15:57:31 +0530

    Update search-in-rotated-sorted-array-ii.java

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+        //bruteforce approach - basically a linear search O(n)
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target) return true;
         }
