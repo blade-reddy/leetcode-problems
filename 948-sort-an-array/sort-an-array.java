5e5e97674ecb611e825d9f045d82b9947c9f767c
blade-reddy <yashwanthr.choodi@gmail.com>
2025-11-14 11:17:43 +0530

    Time: 65 ms (21.86%) | Memory: 75 MB (16.05%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void merge(int arr[],int low, int mid, int high){
+        int left=low;
+        int right=mid+1;
+        List<Integer> temp=new ArrayList<>();
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }else{
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.add(arr[left]);
+                left++;
+        }
+        while( right<=high){
+             temp.add(arr[right]);
+                right++;
+        }
+        for(int i=low;i<=high;i++){
+            arr[i]=temp.get(i-low);
+        }
+    }
+    public void mergesort(int arr[],int low,int high){
+        if(low>=high) return;
+        int mid=(low+high)/2;
+        mergesort(arr,low,mid);
+        mergesort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+    public int[] sortArray(int[] nums) {
+      int n=nums.length;
+      mergesort(nums,0,n-1);
+      return nums;  
+    }
+}
