a4ff388066816776cdf5774b7a511b3bf7f88f76
blade-reddy <yashwanthr.choodi@gmail.com>
2025-11-18 16:09:14 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (22.29%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       int n=nums.length;
+       int[] freq=new int[n+1];
+       for(int num:nums){
+        freq[num]++;
+       }
+       for(int i=0;i<=n;i++){
+        if(freq[i]==0)return i;
+       }
+       return -1;
+    }
+}
