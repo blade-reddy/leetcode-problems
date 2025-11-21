2ff3d90313d94d049933856a92945daf53ca7ec3
blade-reddy <yashwanthr.choodi@gmail.com>
2025-11-21 20:44:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (22.63%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,17 +1,20 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //bruteforce
+        //optimal approach tnaspose the matrix and then reverse
       int n=matrix.length;
-        int[][]ans=new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ans[j][n-1-i]=matrix[i][j];
-            }
-        } 
-         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=ans[i][j];
-            }
-        }     
+      for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            int temp=matrix[i][j];
+            matrix[i][j]=matrix[j][i];
+            matrix[j][i]=temp;
+        }
+      }
+      for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+           int temp=matrix[i][j];
+            matrix[i][j]=matrix[i][n-1-j];
+            matrix[i][n-1-j]=temp;
+        }
+      }  
     }
 }
