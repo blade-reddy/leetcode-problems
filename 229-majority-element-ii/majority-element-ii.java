356727a1a1e5460e2160c3c267edf13210dc44d0
blade-reddy <yashwanthr.choodi@gmail.com>
2025-11-24 14:41:19 +0530

    Time: 19 ms (10.17%) | Memory: 53.1 MB (5.05%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        /*
         int n = nums.length;
         List<Integer> arr=new ArrayList<>();
         for (int i = 0; i < n; i++) {
@@ -11,6 +12,18 @@ class Solution {
          if(count>n/3) arr.add(nums[i]);
           }
            if(arr.size()==2) break; 
+        }
+        return arr;
+        */
+        // using hashing
+         int n=nums.length;
+        List<Integer> arr=new ArrayList<>();
+        Map<Integer,Integer> mpp=new HashMap<>();
+        int min=n/3+1;
+        for(int i=0;i<n;i++){
+            mpp.put(nums[i],mpp.getOrDefault(nums[i],0)+1);
+            if(mpp.get(nums[i])==min) arr.add(nums[i]);
+                    if(arr.size()==2)break;
         }
         return arr;
     }
