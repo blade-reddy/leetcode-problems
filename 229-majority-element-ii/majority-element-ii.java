bc542d5504c3e68d0aaa1e77feb82fd60da7e514
blade-reddy <yashwanthr.choodi@gmail.com>
2025-11-24 16:15:26 +0530

    Time: 3 ms (75.23%) | Memory: 52.4 MB (23.07%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -16,6 +16,7 @@ class Solution {
         return arr;
         */
         // using hashing
+        /*
          int n=nums.length;
         List<Integer> arr=new ArrayList<>();
         Map<Integer,Integer> mpp=new HashMap<>();
@@ -26,5 +27,32 @@ class Solution {
                     if(arr.size()==2)break;
         }
         return arr;
+        */
+        //optimal approach - using cancellation
+        int n=nums.length;
+       int cnt1=0,cnt2=0;
+       int el1=Integer.MIN_VALUE,el2=Integer.MIN_VALUE;
+       for(int i=0;i<n;i++){
+        if(cnt1==0 && el2!=nums[i]){
+            cnt1=1;
+            el1=nums[i];
+        }else if(cnt2==0 && el1!=nums[i]){
+            cnt2=1;
+            el2=nums[i];
+        }else if(el1==nums[i]) cnt1++;
+        else if(el2==nums[i])cnt2++;
+        else {cnt1--;cnt2--;}
+       }
+       cnt1=0;cnt2=0;
+       for(int i=0;i<n;i++){
+        if(el1==nums[i]) cnt1++;
+        if(el2==nums[i])cnt2++;
+       }
+       int min=n/3+1;
+       List<Integer> arr=new ArrayList<>();
+       if(cnt1>=min)  arr.add(el1);
+       if(cnt2>=min && el1!=el2)arr.add(el2);
+       return arr;
+
     }
 }
