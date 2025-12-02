59729f6669fa5458017b21b5bf0be4a922a323b5
blade-reddy <yashwanthr.choodi@gmail.com>
2025-12-02 14:43:56 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (10.38%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,5 +1,14 @@
 class Solution {
+    //recursive approach
+    public int recursive(int[] nums,int low,int high,int target){
+        if(low>high) return -1;
+        int mid=(low+high)/2;
+        if(target==nums[mid]) return mid;
+        else if(target>nums[mid]) return recursive(nums,mid+1,high,target);
+        else return recursive(nums,low,mid-1,target);
+    }
     public int search(int[] nums, int target) {
+        /*
          int low=0,high=nums.length-1;
        while(low<=high){
         int mid=(low+high)/2;
@@ -8,5 +17,7 @@ class Solution {
         else high =mid-1;
        }
        return -1;
+       */
+       return recursive(nums,0,nums.length-1,target);
     }
 }
