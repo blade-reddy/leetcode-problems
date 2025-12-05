a548c02792dfdef862218c93362d45bcec2c2197
blade-reddy <yashwanthr.choodi@gmail.com>
2025-12-05 10:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (10.40%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,5 +1,39 @@
 class Solution {
+    public int lowerBound(int[]nums, int target){
+        int low=0,high=nums.length-1;
+        int ans=nums.length;
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(nums[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public int upperBound(int[]nums, int target){
+        int low=0,high=nums.length-1;
+        int ans=nums.length;
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(nums[mid]>target){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
     public int[] searchRange(int[] nums, int target) {
+        /*
+        //using linear search
        int first=-1,last=-1;
     for(int i=0;i<nums.length;i++){
         if(nums[i]==target){
@@ -8,5 +42,13 @@ class Solution {
         }
     }
     return new int[]{first,last}; 
+    */
+//using upper bound and lower bounds
+ int a=lowerBound(nums,target);
+        if (a==nums.length || nums[a]!=target) return new int[]{-1,-1};
+
+        int b=upperBound(nums,target)-1;
+
+        return new int[]{a,b};
     }
 }
