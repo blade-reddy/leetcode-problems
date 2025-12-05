c4dc968a023efa2aaae01d7cc68a836a169dd56b
blade-reddy <yashwanthr.choodi@gmail.com>
2025-12-05 14:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (9.19%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        //bruteforce approach - basically a linear search O(n)
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target) return true;
         }
