542387941b7cc431e3135d9ba2d01ab4ec0d87b9
blade-reddy <yashwanthr.choodi@gmail.com>
2025-12-08 15:57:25 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (18.46%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,36 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        /*
+         if(nums.length==1) return nums[0];
+      for(int i=0;i<nums.length;i++){
+        if(i==0){
+            if(nums[0]!=nums[i+1]) return nums[i];
+        }else if (i==nums.length-1){
+            if(nums[nums.length-1]!=nums[nums.length-2]) return nums[nums.length-1];
+        }else{
+            if (nums[i]!=nums[i+1] && nums[i]!=nums[i-1]) return nums[i];
+        }
+      }
+      return -1;
+      //using XOR
         int n=0;
       for(int i=0;i<nums.length;i++){
        n=nums[i]^n;
       }
       return n;
+      */
+      // using binary search-(even,odd) indices concept
+      if (nums.length == 1) return nums[0];
+    if (nums[0] != nums[1]) return nums[0];
+    if (nums[nums.length - 1] != nums[nums.length - 2]) return nums[nums.length - 1];
+    int low = 1, high = nums.length - 2;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
+      if ((mid % 2 == 1 && nums[mid - 1] == nums[mid])
+          || mid % 2 == 0 && nums[mid + 1] == nums[mid]) low = mid + 1;
+      else high = mid - 1;
+    }
+    return -1;
     }
 }
