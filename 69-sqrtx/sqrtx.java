f8dd00cf7c4f9db265068c02848ee396b2ddd775
blade-reddy <yashwanthr.choodi@gmail.com>
2025-12-12 17:20:39 +0530

    Time: 1 ms (93.70%) | Memory: 42.4 MB (56.89%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,6 @@
 class Solution {
     public int mySqrt(int x) {
+        /*
         int ans = 0;
         for (int i = 1; i <= x; i++) {
             if ((long) i * (long) i <= x)
@@ -9,5 +10,15 @@ class Solution {
                 break;
         }
         return ans;
+        */
+        //using binary search
+       int low=1,high=x;
+      while(low<=high){
+        int mid=low+(high-low)/2;
+        long val=(long)mid*(long)mid;
+        if(val<=(long)x)low=mid+1; 
+        else high=mid-1;
+      }
+      return high;
     }
 }
