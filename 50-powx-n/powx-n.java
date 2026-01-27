15c2acfd38f24b5171005ffbe2f2fd3cf464cee5
blade-reddy <yashwanthr.choodi@gmail.com>
2026-01-27 13:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (7.72%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,13 @@
 class Solution {
+    private double myPows(double x, long n){
+        if(n==0) return 1;
+        if(n==1) return x;
+        if(n%2==0) return myPows(x*x,n/2);
+        return x*myPows(x,n-1);
+    }
     public double myPow(double x, int n) {
-           return Math.pow(x,n);
+          long num=n;
+          if(num<0) return (1.0/myPows(x,-num));
+        return myPows(x,num);
     }
 }
