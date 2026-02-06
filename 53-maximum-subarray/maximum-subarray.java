930094add2d4fe9e56675f6eeefcc3a9473de6fa
blade-reddy <yashwanthr.choodi@gmail.com>
2026-02-06 10:56:08 +0530

    Time: 1 ms (99.59%) | Memory: 77.1 MB (80.39%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,5 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        /*
+         int maxi=Integer.MIN_VALUE;;
+        for(int i=0;i<nums.length;i++){
+                int sum = 0;
+                for (int k = i; k < nums.length; k++) {
+                    sum += nums[k];
+                maxi = Math.max(maxi, sum);
+                }
+            }
+        return maxi;
+           */
         //kadane's algorithm
         long max=Long.MIN_VALUE;
         long sum=0;
@@ -11,5 +22,6 @@ class Solution {
                 if(sum<0) sum=0;
             }
         return (int)max;
+     
     }
 }
