83f33316a0077f8e7415bce83202bed496d915fb
blade-reddy <yashwanthr.choodi@gmail.com>
2026-03-03 10:36:24 +0530

    Time: 5 ms (35.44%) | Memory: 47.1 MB (10.45%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character,Integer> mpp=new HashMap<>();
+        mpp.put('I',1);
+        mpp.put('V',5);
+        mpp.put('X',10);
+        mpp.put('L',50);
+        mpp.put('C',100);
+        mpp.put('D',500);
+        mpp.put('M',1000);
+     int result=mpp.get(s.charAt(s.length()-1));
+     for(int i=s.length()-2;i>=0;i--){
+        if(mpp.get(s.charAt(i))<mpp.get(s.charAt(i+1))) result-=mpp.get(s.charAt(i));
+        else result+=mpp.get(s.charAt(i));
+     }
+     return result;
+    }
+}
